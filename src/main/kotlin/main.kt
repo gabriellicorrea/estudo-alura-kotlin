1154059ed80a3725be826cc384aba7fe79cc5c17
Gabrielli Correa <gabriellicorrea7@gmail.com>
2021-06-07 18:16:08 -0300

    estudos com kotlin

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,6 +2,20 @@ fun main() {
     println("hello word")
 
     val titular = "Gabi"
+    val numeroConta = 1000
+    var saldo = 0.0
+    saldo = 1000.0 + 500
+    saldo -= 5000
 
     println("Titular da conta $titular")
+    println("Numero da conta $numeroConta")
+    println("Saldo da conta $saldo")
+
+    if(saldo > 0.0) {
+        println("Saldo positivo")
+    }else if(saldo  == 0.0){
+        println("Saldo neutro")
+    }else {
+        println("Saldo negativo")
+    }
 }
